7e65ca469f4fb88b8a28054e65362b0d1495680e
Shikhar Sharma <shikhar.sharma1496@gmail.com>
2022-10-07 02:32:13 -0400

    debug t2.

--- wsddn.py
+++ wsddn.py
@@ -134,10 +134,10 @@ class WSDDN(nn.Module):
         # essentially we would have the sum of probabilities for each of classes
         # calculate the BCE loss on the label vector and the probabilities
         # BCE loss can handle he probabilities, 
-        cls_prob = torch.sum(cls_prob, dim=0)
-        print("printiingLOSSSSS:::::::::")
-        print(cls_prob.shape)
-        print(label_vec.shape)
+        cls_prob = torch.reshape(torch.sum(cls_prob, dim=0),(-1,1))
+        # print("printiingLOSSSSS:::::::::")
+        # print(cls_prob.shape)
+        # print(label_vec.shape)
         # cls_prob = F.softmax(cls_prob)
         loss = nn.BCELoss(cls_prob, label_vec, reduction='sum').to(device)
 
