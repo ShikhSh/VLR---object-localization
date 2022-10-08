d816fe94e2d7035007cb20d2a4429072e57932a0
Shikhar Sharma <shikhar.sharma1496@gmail.com>
2022-10-07 22:53:56 -0400

    debug t2.

--- wsddn.py
+++ wsddn.py
@@ -103,7 +103,8 @@ class WSDDN(nn.Module):
         score1 = torch.softmax(self.bbox_fc(lin_model_out), dim = 0)
         score2 = torch.softmax(self.score_fc(lin_model_out), dim = 1)
 
-        cls_prob = torch.mul(score1, score2)
+        # cls_prob = torch.mul(score1, score2)
+        cls_prob = torch.mul(score1, score2).clamp(0.0, 1.0)
 
 
         if self.training:
@@ -130,107 +131,13 @@ class WSDDN(nn.Module):
         # essentially we would have the sum of probabilities for each of classes
         # calculate the BCE loss on the label vector and the probabilities
         # BCE loss can handle he probabilities, 
-        cls_prob = torch.reshape(torch.sum(cls_prob, dim=0).clamp(0.0, 1.0),(-1,1))
         # print(cls_prob)
         # print(label_vec)
         # print("printiingLOSSSSS:::::::::")
         # print(cls_prob.shape)
         # print(label_vec.shape)
         # cls_prob = F.softmax(cls_prob)
+        cls_prob = torch.reshape(torch.sum(cls_prob, dim=0),(-1,1))
         loss = F.binary_cross_entropy(cls_prob, label_vec, reduction='sum')#.to(device)
 
         return loss
-
-    # def __init__(self, classes=classes):
-    #     super(WSDDN, self).__init__()
-
-    #     print(f'classes {classes}')
-
-    #     if classes is not None:
-    #         self.classes = classes
-    #         self.n_classes = len(classes)
-    #         print(classes)
-
-    #     # TODO (Q2.1): Define the WSDDN model
-    #     self.features = nn.Sequential(
-    #       nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2)),
-    #       nn.ReLU(inplace=True),
-    #       nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), dilation=(1, 1), ceil_mode=False),
-    #       nn.Conv2d(64, 192, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2)),
-    #       nn.ReLU(inplace=True),
-    #       nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2), dilation=(1, 1), ceil_mode=False),
-    #       nn.Conv2d(192, 384, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-    #       nn.ReLU(inplace=True),
-    #       nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-    #       nn.ReLU(inplace=True),
-    #       nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-    #       nn.ReLU(inplace=True)
-    #     )
-    #     self.roi_pool = roi_pool
-    #     self.classifier =  nn.Sequential(
-    #         nn.Dropout(p=0.5),
-    #         nn.Linear(in_features=6*6*256, out_features=4096),
-    #         nn.ReLU(inplace=True),
-    #         nn.Dropout(),
-    #         nn.Linear(4096, 4096),
-    #         nn.ReLU(inplace=True),
-    #     )
-
-    #     self.score_fc   = nn.Linear(4096, len(classes))
-    #     self.bbox_fc    = nn.Linear(4096, len(classes))
-
-    #     # loss
-    #     self.cross_entropy = None
-
-    # @property
-    # def loss(self):
-    #     return self.cross_entropy
-
-    # def forward(self,
-    #             image,
-    #             rois=None,
-    #             gt_vec=None,
-    #             ):
-    #     # image: [1, 3, 512, 512]
-
-    #     # TODO (Q2.1): Use image and rois as input
-    #     # compute cls_prob which are N_roi X 20 scores
-    #     inp_size = image.shape[-1]*1.0
-    #     x = self.features(image)
-    #     feature_dim = x.shape[-1]*1.0
-
-    #     # print(f'rois.shape {rois.shape}')
-    #     rois = rois.squeeze()
-    #     x = self.roi_pool(x, [rois.type('torch.FloatTensor').cuda()], output_size=6, spatial_scale=feature_dim/inp_size) # 300x256x7x7 [N_roi X C X OUT X OUT]
-    #     x = x.flatten(start_dim=1)
-    #     x = self.classifier(x) # (N_roi X 4096)
-        
-    #     class_score = self.score_fc(x) # (N_roi X 20)
-    #     bbox = self.bbox_fc(x) # (N_roi X 20)
-        
-    #     class_score = torch.softmax(class_score, dim = 1) # (N_roi X 20)
-    #     bbox = torch.softmax(bbox, dim = 0) # (N_roi X 20)
-
-    #     cls_prob = class_score*bbox # (N_roi X 20) [for each roi we have a prob of that roi belonging to a particular class]
-
-
-    #     if self.training:
-    #         label_vec = gt_vec.view(self.n_classes, -1)
-    #         self.cross_entropy = self.build_loss(cls_prob, label_vec)
-    #     return cls_prob
-
-    # def build_loss(self, cls_prob, label_vec):
-    #     """Computes the loss
-
-    #     :cls_prob: N_roix20 output scores
-    #     :label_vec: 1x20 one hot label vector
-    #     :returns: loss
-
-    #     """
-    #     # TODO (Q2.1): Compute the appropriate loss using the cls_prob
-    #     # that is the output of forward()
-    #     # Checkout forward() to see how it is called
-    #     cls_prob = torch.sum(cls_prob, dim=0).unsqueeze(1)
-    #     cls_prob = torch.clamp(cls_prob, 0., 1.)
-    #     loss = F.binary_cross_entropy(cls_prob, label_vec, reduction='sum')
-    #     return loss
